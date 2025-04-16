2aa5f2d7cd7dc8e3afe40398ed08b9c4b262c452
abhishek-kumar2 <abhishek.kr.indira@gmail.com>
2025-04-16 22:33:50 +0800

    navbar and footer components added

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,5 @@
+export default function BlogCard() {
+    return (
+        <div className="p-4 border border-gray-400 rounded">Blog Post Preview</div>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-gray-500 p-4">Navbar</nav>
+    )
+}
